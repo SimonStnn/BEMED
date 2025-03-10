fed27f061cfb1cb4aca3d9fd6839ca10df7ce1b3
SimonStnn <simon.stijnen.23@gmail.com>
2025-03-10 19:12:56 +0100

    Correctly load env to frontend

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -14,3 +14,22 @@ export function joinPaths(...paths: string[]): string {
 export function getAPIUrl(path?: string): string {
   return joinPaths("http://localhost:8164", path || "");
 }
+
+export const requiredEnvVars = [
+  "KEYCLOAK_FRONTEND_CLIENT_ID",
+  "KEYCLOAK_BACKEND_CLIENT_ID",
+  "KEYCLOAK_CLIENT_SECRET",
+  "KEYCLOAK_REALM",
+  "DOMAIN",
+  "DNS_PREFIX_KEYCLOAK",
+  "DNS_PREFIX_BEMED_BACKEND",
+] as const;
+
+export type EnvVariable = (typeof requiredEnvVars)[number];
+
+const { DNS_PREFIX_KEYCLOAK, DNS_PREFIX_BEMED_BACKEND, DOMAIN } =
+  process.env as Record<EnvVariable, string>;
+export const url = {
+  keycloak: new URL(`http://${DNS_PREFIX_KEYCLOAK}.${DOMAIN}`),
+  backend: new URL(`http://${DNS_PREFIX_BEMED_BACKEND}.${DOMAIN}`),
+} as const;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,55 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import dotenv from "dotenv";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+import path from "path";
+
+import { requiredEnvVars } from "./src/utils";
+
+const env: { [key: string]: string } = {};
+
+export function loadEnv() {
+  const envPath = path.resolve(__dirname, "..", ".env");
+  dotenv.config({
+    path: envPath,
+    encoding: "utf8",
+    processEnv: env,
+  });
+
+  class EnvError extends Error {
+    constructor(message: string) {
+      super(message + " environment variable is required");
+    }
+  }
+
+  for (const envVar of requiredEnvVars) {
+    if (!env[envVar]) {
+      throw new EnvError(envVar);
+    }
+  }
+
+  // Remove variables not in requiredEnvVars
+  Object.keys(env).forEach((key) => {
+    if (!requiredEnvVars.includes(key as (typeof requiredEnvVars)[number])) {
+      delete env[key];
+    }
+  });
+
+  console.debug(`Loaded environment variables from ${envPath}`);
+}
+loadEnv();
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  define: {
+    "process.env": env,
+  },
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
