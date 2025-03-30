4fcbade7e0433b17541cb23d08996035e2fdff98
SimonStnn <simon.stijnen.23@gmail.com>
2025-03-30 14:59:59 +0200

    Update relationships

--- config/db/restore.sql
+++ config/db/restore.sql
@@ -1,77 +1,63 @@
 use "BEMED";
-
 -- Users Table
-CREATE TABLE
-    users (
-        id INT PRIMARY KEY,
-        name VARCHAR(255) UNIQUE NOT NULL
-    );
-
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) UNIQUE NOT NULL
+);
 -- Treatments Table
-CREATE TABLE
-    treatments (
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        userId INT NOT NULL,
-        createdAt DATE NOT NULL DEFAULT CURRENT_DATE,
-        updatedAt DATE NOT NULL DEFAULT CURRENT_DATE,
-        FOREIGN KEY (userId) REFERENCES users (id)
-    );
-
+CREATE TABLE treatments (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    userId INT NOT NULL,
+    createdAt DATE NOT NULL DEFAULT CURRENT_DATE,
+    updatedAt DATE NOT NULL DEFAULT CURRENT_DATE,
+    FOREIGN KEY (userId) REFERENCES users (id)
+);
 -- Questions Table
-CREATE TABLE
-    questions (
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        question VARCHAR(255) NOT NULL,
-        type VARCHAR(50) NOT NULL,
-    );
-
+CREATE TABLE questions (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    question VARCHAR(255) NOT NULL,
+    type VARCHAR(50) NOT NULL
+);
 -- Answers Table
-CREATE TABLE
-    answers (
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        questionId INT NOT NULL,
-        treatmentId INT NOT NULL,
-        answer VARCHAR(255) NOT NULL,
-        FOREIGN KEY (questionId) REFERENCES questions (id) FOREIGN KEY (treatmentId) REFERENCES treatments (id)
-    );
-
+CREATE TABLE answers (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    questionId INT NOT NULL,
+    treatmentId INT NOT NULL,
+    answer VARCHAR(255) NOT NULL,
+    FOREIGN KEY (questionId) REFERENCES questions (id),
+    FOREIGN KEY (treatmentId) REFERENCES treatments (id)
+);
 -- Products Table
-CREATE TABLE
-    products (
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        name VARCHAR(255) NOT NULL,
-        price FLOAT NOT NULL,
-        weight FLOAT NOT NULL,
-        EF FLOAT NOT NULL,
-        alternativeId INT,
-        FOREIGN KEY (alternativeId) REFERENCES alternatives (id)
-    );
-
--- Assesments Table
-CREATE TABLE
-    assesments (
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        userId INT NOT NULL,
-        ppm INT NOT NULL,
-        productId INT NOT NULL,
-        createdAt DATE NOT NULL DEFAULT CURRENT_DATE,
-        updatedAt DATE NOT NULL DEFAULT CURRENT_DATE,
-        FOREIGN KEY (userId) REFERENCES users (id),
-        FOREIGN KEY (productId) REFERENCES alternatives (id)
-    );
-
+CREATE TABLE products (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NULL,
+    price FLOAT NOT NULL,
+    weight FLOAT NOT NULL,
+    EF FLOAT NOT NULL
+);
 -- Alternatives Table
-CREATE TABLE
-    alternatives (
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        productId INT NOT NULL,
-        FOREIGN KEY (productId) REFERENCES products (id) ON DELETE CASCADE
-    );
-
-INSERT INTO
-    questions (question, type)
-VALUES
-    (
+CREATE TABLE alternatives (
+    productId INT NOT NULL,
+    alternativeId INT NOT NULL,
+    PRIMARY KEY (productId, alternativeId),
+    FOREIGN KEY (productId) REFERENCES products (id) ON DELETE CASCADE,
+    FOREIGN KEY (alternativeId) REFERENCES products (id) ON DELETE CASCADE
+);
+-- Assesments Table
+CREATE TABLE assesments (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    userId INT NOT NULL,
+    productId INT NOT NULL,
+    ppm INT NOT NULL,
+    createdAt DATE NOT NULL DEFAULT CURRENT_DATE,
+    updatedAt DATE NOT NULL DEFAULT CURRENT_DATE,
+    FOREIGN KEY (userId) REFERENCES users (id),
+    FOREIGN KEY (productId) REFERENCES products (id)
+);
+-- Insert Questions
+INSERT INTO questions (question, type)
+VALUES (
         'Does your institution currently practice waste separation?',
         'text'
     ),
@@ -99,17 +85,18 @@ VALUES
         'What is the total amount of separately collected waste? Approximation in KG',
         'number'
     );
-
-INSERT INTO
-    alternatives (id, productId)
-VALUES
-    (1, 1);
-
-(2, 1);
-
-INSERT INTO
-    products (name, price, weight, EF, alternativeId)
-VALUES
-    ('Plastic straws', 0.10, 0.01, 0.5, NULL),
-    ('Paper straws', 0.15, 0.01, 0.2, 1),
-    ('Reusable straws', 0.50, 0.25, 0.1, 2);
+-- Insert Products
+INSERT INTO products (id, name, price, weight, EF)
+VALUES (1, 'Plastic straws', 0.10, 0.01, 0.5),
+    (2, 'Paper straws', 0.15, 0.01, 0.2),
+    (3, 'Reusable straws', 0.50, 0.25, 0.1);
+-- Insert Alternatives
+INSERT INTO alternatives (productId, alternativeId)
+VALUES -- plastic straws can be replaced by paper straws
+    (1, 2),
+    -- plastic straws can be replaced by reusable straws
+    (1, 3),
+    -- paper straws can be replaced by reusable straws
+    (2, 3),
+    -- reusable straws can be replaced by paper straws
+    (3, 2);
