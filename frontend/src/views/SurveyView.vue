556cfb36520768e06f3c652a913e3d44d4e8bac4
Marea van der Wiel <727203@student.inholland.nl>
2025-04-02 11:13:11 +0200

    Changes to the UI from survey

--- frontend/src/views/SurveyView.vue
+++ frontend/src/views/SurveyView.vue
@@ -24,69 +24,63 @@ function onSuccess(data: any) {
 </script>
 
 <template>
-  <v-container class="survey">
-    <v-row justify="center">
-      <v-col cols="12" md="12">
-        <v-card class="survey-card">
-          <v-card-title>
-            <span class="headline">Survey Page</span>
-          </v-card-title>
-          <v-card-text class="survey-content">
-            <api-form action="/survey" method="POST" v-on:success="onSuccess" v-bind:body="formData"
-            @submit="checkFormData">
-              
-              <!--Yes/no questions (4)-->
-              <v-radio-group v-model="formData.questions[1]" class="survey-field" name="q1" required>
-                <p class="question-label">Does your institution currently practice waste separation?</p>
-                <v-radio label="Yes" value="Yes"></v-radio>
-                <v-radio label="No" value="No"></v-radio>
-                <v-radio label="I don't know" value="I don't know"></v-radio>
-              </v-radio-group>
+  <v-card class="survey-card">
+    <v-card-title>
+      <span class="headline">Survey Page</span>
+    </v-card-title>
+    <v-card-text class="survey-content">
+      <api-form action="/survey" method="POST" v-on:success="onSuccess" v-bind:body="formData"
+      @submit="checkFormData">
+        
+        <!--Yes/no questions (4)-->
+        <v-radio-group v-model="formData.questions[1]" class="survey-field" name="q1" required>
+          <p class="question-label">Does your institution currently practice waste separation?</p>
+          <v-radio label="Yes" value="Yes"></v-radio>
+          <v-radio label="No" value="No"></v-radio>
+          <v-radio label="I don't know" value="I don't know"></v-radio>
+        </v-radio-group>
 
-              <v-radio-group v-model="formData.questions[2]" class="survey-field" name="q2" required>
-                <p class="question-label">Does your institution have special bins or containers for separate waste collection?</p>
-                <v-radio label="Yes" value="Yes"></v-radio>
-                <v-radio label="No" value="No"></v-radio>
-                <v-radio label="I don't know" value="I don't know"></v-radio>
-              </v-radio-group>
+        <v-radio-group v-model="formData.questions[2]" class="survey-field" name="q2" required>
+          <p class="question-label">Does your institution have special bins or containers for separate waste collection?</p>
+          <v-radio label="Yes" value="Yes"></v-radio>
+          <v-radio label="No" value="No"></v-radio>
+          <v-radio label="I don't know" value="I don't know"></v-radio>
+        </v-radio-group>
 
-              <v-radio-group v-model="formData.questions[3]" class="survey-field" name="q3" required>
-                <p class="question-label">Does your institution have established policies or guidelines for waste management?</p>
-                <v-radio label="Yes" value="Yes"></v-radio>
-                <v-radio label="No" value="No"></v-radio>
-                <v-radio label="I don't know" value="I don't know"></v-radio>
-              </v-radio-group>
+        <v-radio-group v-model="formData.questions[3]" class="survey-field" name="q3" required>
+          <p class="question-label">Does your institution have established policies or guidelines for waste management?</p>
+          <v-radio label="Yes" value="Yes"></v-radio>
+          <v-radio label="No" value="No"></v-radio>
+          <v-radio label="I don't know" value="I don't know"></v-radio>
+        </v-radio-group>
 
-              <v-radio-group v-model="formData.questions[4]" class="survey-field" name="q4" required>
-                <p class="question-label">Does your institution conduct training to raise awareness about waste management?</p>
-                <v-radio label="Yes" value="Yes"></v-radio>
-                <v-radio label="No" value="No"></v-radio>
-                <v-radio label="I don't know" value="I don't know"></v-radio>
-              </v-radio-group>
+        <v-radio-group v-model="formData.questions[4]" class="survey-field" name="q4" required>
+          <p class="question-label">Does your institution conduct training to raise awareness about waste management?</p>
+          <v-radio label="Yes" value="Yes"></v-radio>
+          <v-radio label="No" value="No"></v-radio>
+          <v-radio label="I don't know" value="I don't know"></v-radio>
+        </v-radio-group>
 
-              <!--Numeric questions-->
-              <div class="survey-field">
-              <p class="num-question-label">How much single-use plastic packaging has your institution collected since the beginning of the year? Approximation in KG</p>
-              <v-text-field v-model.number="formData.questions[5]" type="number" label="Q5" name="q5" required></v-text-field></div>
+        <!--Numeric questions-->
+        <div class="survey-field">
+        <p class="num-question-label">How much single-use plastic packaging has your institution collected since the beginning of the year? Approximation in KG</p>
+        <v-text-field v-model.number="formData.questions[5]" type="number" label="Q5" name="q5" required></v-text-field></div>
 
-              <div class="survey-field">
-              <p class="num-question-label">How much paper (if collected separately) has your institution gathered so far? Approximation in KG</p>
-              <v-text-field v-model.number="formData.questions[6]" type="number" label="Q6" name="q6"></v-text-field></div>
+        <div class="survey-field">
+        <p class="num-question-label">How much paper (if collected separately) has your institution gathered so far? Approximation in KG</p>
+        <v-text-field v-model.number="formData.questions[6]" type="number" label="Q6" name="q6"></v-text-field></div>
 
-              <div class="survey-field">
-              <p class="num-question-label">What is the total amount of separately collected waste? Approximation in KG</p>
-              <v-text-field v-model.number="formData.questions[7]" type="number" label="Q7" name="q7" required></v-text-field></div>
+        <div class="survey-field">
+        <p class="num-question-label">What is the total amount of separately collected waste? Approximation in KG</p>
+        <v-text-field v-model.number="formData.questions[7]" type="number" label="Q7" name="q7" required></v-text-field></div>
 
-              <!--Submit survey-->
-              <v-btn type="submit" color="primary">Submit</v-btn>
+        <!--Submit survey-->
+        <v-btn type="submit" color="primary">Submit</v-btn>
 
-            </api-form>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
-  </template>
+      </api-form>
+    </v-card-text>
+  </v-card>
+</template>
   
 <script lang="ts">
 </script>
@@ -98,8 +92,14 @@ function onSuccess(data: any) {
 }
 
 .survey-card {
-  max-height: 80vh;
-  overflow-y: auto; /* to enable scrolling */
+  width: 100%;
+  height: 100vh;
+  flex-direction: column;
+}
+
+.survey-content {
+  flex-grow: 1;
+  padding: 20px;
 }
 
 /* to make survey fields look similar */
