41de7329ea8a1986285d67069167caa62a404b53
SimonStnn <simon.stijnen.23@gmail.com>
2025-04-10 08:45:34 +0200

    Add swagger ui to serve api docs

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 
+import docsRouter from "@/routes/docs";
 import treatmentRouter from "@/routes/treatment";
 import productRouter from "@/routes/product";
 import assessmentRouter from "@/routes/assessment";
 
 const router = Router();
 
+
 router.use("/assessment", assessmentRouter);
+router.use("/docs", docsRouter); // Swagger UI
 router.use("/product", productRouter);
 router.use("/treatment", treatmentRouter);
 
